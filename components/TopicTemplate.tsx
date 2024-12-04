338eba455fe838c793de59dccbdfe966f5fed9d3
Coding-Cooking <fei.coding2023@gmail.com>
2024-12-04 18:52:17 +1100

    add prev and next button

--- components/TopicTemplate.tsx
+++ components/TopicTemplate.tsx
@@ -2,6 +2,7 @@
 
 import { TopicType } from "@/model/Topic";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 type TopicTemplateProps = {
@@ -32,58 +33,67 @@ export default function TopicTemplate({ topic }: TopicTemplateProps) {
     }
 
     return (
-        <div className="w-3/5 h-screen flex flex-col items-center mx-auto my-4">
-            <p className="my-4">{topic.question.questionContent}</p>
-            {topic.question.questionImage && (<Image src={topic.question.questionImage} alt={'question image'} width={100} height={100} />)}
-            <form onSubmit={handleSubmit}>
-                {
-                    Object.entries(topic.options).map(([key, value]) =>
-                        <label
-                            key={`${key}-${value}`}
-                            className={`block flex gap-2 my-2 p-3 cursor-pointer
+        <div className="w-full h-full flex justify-center items-center">
+            {Number(topic.topicId) > 1 && (<Link href={`/saa/${Number(topic.topicId) - 1}`}>
+                <Image src='/prevIcon.svg' alt='prev one' width={60} height={60} />
+            </Link>)}
+
+            <div className="w-4/5 md:w-3/5 h-screen flex flex-col items-center mx-auto my-4">
+                <p className="my-4">{topic.question.questionContent}</p>
+                {topic.question.questionImage && (<Image src={topic.question.questionImage} alt={'question image'} width={100} height={100} />)}
+                <form onSubmit={handleSubmit}>
+                    {
+                        Object.entries(topic.options).map(([key, value]) =>
+                            <label
+                                key={`${key}-${value}`}
+                                className={`block flex gap-2 my-2 p-3 cursor-pointer
                             ${selectedOptions.includes(key) ? 'text-slate-950 bg-blue-200' : 'hover:bg-blue-200 hover:text-slate-950'}
                             ${topic.correctAnswers.length === 1 &&
-                                    selectedOptions[0] === key &&
-                                    topic.correctAnswers.includes(key)
-                                    ? 'bg-emerald-400 text-slate-950'
-                                    : topic.correctAnswers.length === 1 &&
-                                        selectedOptions.length !== 0 &&
-                                        selectedOptions[0] !== key &&
+                                        selectedOptions[0] === key &&
                                         topic.correctAnswers.includes(key)
                                         ? 'bg-emerald-400 text-slate-950'
                                         : topic.correctAnswers.length === 1 &&
-                                            selectedOptions[0] == key &&
-                                            !topic.correctAnswers.includes(key)
-                                            ? 'bg-red-300 text-slate-950'
-                                            : ''
-                                }
+                                            selectedOptions.length !== 0 &&
+                                            selectedOptions[0] !== key &&
+                                            topic.correctAnswers.includes(key)
+                                            ? 'bg-emerald-400 text-slate-950'
+                                            : topic.correctAnswers.length === 1 &&
+                                                selectedOptions[0] == key &&
+                                                !topic.correctAnswers.includes(key)
+                                                ? 'bg-red-300 text-slate-950'
+                                                : ''
+                                    }
                             ${topic.correctAnswers.length > 1 && submitted && topic.correctAnswers.includes(key) ? 'bg-emerald-400 text-slate-950' : topic.correctAnswers.length > 1 && submitted && !topic.correctAnswers.includes(key) && selectedOptions.includes(key) ? 'bg-red-300 text-slate-950' : ''}
 
                             `}
-                            onClick={(e) => { handleClick(e, key) }}
-                        >
-                            <input type={topic.correctAnswers.length > 1 ? 'checkbox' : 'radio'}
-                                name={topic.correctAnswers.length > 1 ? 'options[]' : 'options'}
-                                value={key}
-                                className="hidden"
-                            />
-                            <div>{key}.</div>
-                            <div>
-                                {value.optionContent}
-                                {value.optionImage && (
-                                    <Image src={value.optionImage} alt="option image" width={100} height={100} />
-                                )}
-                            </div>
-                        </label>
-                    )
-                }
-                {topic.correctAnswers.length > 1 && (
-                    <button type="submit" className="block mx-auto my-4 bg-blue-500 text-white px-4 py-2 rounded">
-                        Submit
-                    </button>
-                )}
-            </form>
-
+                                onClick={(e) => { handleClick(e, key) }}
+                            >
+                                <input type={topic.correctAnswers.length > 1 ? 'checkbox' : 'radio'}
+                                    name={topic.correctAnswers.length > 1 ? 'options[]' : 'options'}
+                                    value={key}
+                                    className="hidden"
+                                />
+                                <div>{key}.</div>
+                                <div>
+                                    {value.optionContent}
+                                    {value.optionImage && (
+                                        <Image src={value.optionImage} alt="option image" width={100} height={100} />
+                                    )}
+                                </div>
+                            </label>
+                        )
+                    }
+                    {topic.correctAnswers.length > 1 && (
+                        <button type="submit" className="block mx-auto my-4 bg-blue-500 text-white px-4 py-2 rounded">
+                            Submit
+                        </button>
+                    )}
+                </form>
+            </div >
+            <Link href={`/saa/${Number(topic.topicId) + 1}`}>
+                <Image src='/nextIcon.svg' alt='next one' width={60} height={60} />
+            </Link>
         </div >
+
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens:{
+      'md': '768px'
+    },
     extend: {
       colors: {
         background: "var(--background)",
