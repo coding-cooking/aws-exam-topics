aa34e581f69c468c43892304d3fe6811d8d9c0c2
Coding-Cooking <fei.coding2023@gmail.com>
2024-12-05 21:12:51 +1100

    update the topic list style

--- app/saa/page.tsx
+++ app/saa/page.tsx
@@ -2,13 +2,13 @@
 
 import TopicsContext, { TopicType } from "@/context/TopicsContext";
 import Link from "next/link";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
 export default function SaaPage() {
     const topics: TopicType[] = useContext(TopicsContext)
 
     return (
-        <div className="flex items-start justify-start gap-8 px-4 py-6 font-[family-name:var(--font-geist-sans)]">
+        <div className="w-4/5 h-full mx-auto flex flex-wrap items-start justify-start gap-8 px-4 py-6">
             {topics && (topics.map(item => (<div key={item._id}>
                 <Link href={`/saa/${item.topicId}`}>
                     <div className="flex items-center justify-center w-6 h-6 border border-solid border-1 border-black rounded-xl">{item.topicId}</div>
