65a4c8b98e238d53863e388a72d76328b98b3e5f
Coding-Cooking <fei.coding2023@gmail.com>
2024-12-06 21:51:13 +1100

    start build auth

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,20 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+    ],
+    session: {
+        strategy: 'jwt',
+    },
+    callbacks: {
+
+       
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+
+}
