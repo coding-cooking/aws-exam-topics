696a4ddb800199f4a75219b52340d6f06c8b82a5
Coding-Cooking <fei.coding2023@gmail.com>
2025-01-02 20:35:19 +1100

    resolve the issue, only after user actually paid they can get activation code and cart be empty

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,11 +1,8 @@
 import Stripe from 'stripe';
 import { NextRequest, NextResponse } from 'next/server';
 import { checkoutItemType } from '@/app/cart/page';
-import { dbConnect } from '@/utils/dbConnect';
 import { getServerSession } from 'next-auth';
 import { options } from '../auth/[...nextauth]/options';
-import User, { CartItemType } from '@/model/User';
-import { generateActivationCode } from '@/utils/generateActivationCode';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
@@ -13,6 +10,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
     try {
         const body = await req.json()
         const { items } = body;
+        const session = await getServerSession(options);
 
         const successParams = new URLSearchParams({ items: JSON.stringify(items) }).toString();
 
@@ -26,41 +24,48 @@ export async function POST(req: NextRequest, res: NextResponse) {
             };
         });
 
+        if (!session) {
+            return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+        }
+
         const checkoutsession = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             line_items: lineItems,
             mode: 'payment',
             success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout/checkout-success?${successParams}`,
             cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout/checkout-cancel`,
-        });
-        
-        await dbConnect();
-        const session = await getServerSession(options);
-        if (session) {
-            const userId = session.user.id;
-            const user = await User.findById(userId);
-            if (Array.isArray(user.cart)) {
-                items.forEach((item: checkoutItemType) => {
-                    //after checkout, add product(s) to user
-                    user.subscriptionProducts.push({
-                        product: item.name,
-                        activationDate: null,
-                        expirationDate: null,
-                    })
-                    //after checkout, add activationCode to user
-                    const activationCode = generateActivationCode();
-                    user.activationInfos.push({
-                        code: activationCode,
-                        product: item.name,
-                        used: false,
-                    });
-                    //after checkout, delete the items in cart
-                    user.cart = user.cart.filter((i: CartItemType) => i._id.toString() !== item.id);
-                })
-                await user.save();
+            metadata: {
+                userId: session.user.id,
             }
-        }
+        });
         return NextResponse.json({ url: checkoutsession.url }, { status: 200 });
+
+        // await dbConnect();
+        // if (session) {
+        //     const userId = session.user.id;
+        //     const user = await User.findById(userId);
+        //     if (Array.isArray(user.cart)) {
+        //         items.forEach((item: checkoutItemType) => {
+        //             //after checkout, add product(s) to user
+        //             user.subscriptionProducts.push({
+        //                 product: item.name,
+        //                 activationDate: null,
+        //                 expirationDate: null,
+        //             })
+        //             //after checkout, add activationCode to user
+        //             const activationCode = generateActivationCode();
+        //             user.activationInfos.push({
+        //                 code: activationCode,
+        //                 product: item.name,
+        //                 used: false,
+        //             });
+        //             //after checkout, delete the items in cart
+        //             user.cart = user.cart.filter((i: CartItemType) => i._id.toString() !== item.id);
+        //         })
+        //         await user.save();
+        //     }
+        // }
+
     } catch (err: unknown) {
         if (err instanceof Stripe.errors.StripeError) {
             console.error('Stripe session creation error:', err);
---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,94 @@
+import { dbConnect } from '@/utils/dbConnect';
+import { NextRequest, NextResponse } from 'next/server';
+import Stripe from 'stripe';
+import User, { CartItemType } from '@/model/User';
+import { generateActivationCode } from '@/utils/generateActivationCode';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    const sig = req.headers.get('stripe-signature');
+
+    if (!sig) {
+        return NextResponse.json(
+            { message: 'No signature found' },
+            { status: 400 }
+        );
+    }
+
+    try {
+        const body = await req.text();
+        const event = stripe.webhooks.constructEvent(
+            body,
+            sig,
+            endpointSecret
+        );
+        console.log('event.type are', event.type)
+        // Handle the event
+        switch (event.type) {
+            case 'payment_intent.created':
+                break;
+            case 'payment_intent.succeeded':
+                break;
+            case 'charge.succeeded':
+                break;
+            case 'checkout.session.completed':
+                // Handle successful payment
+                const checkoutSessionCompleted = event.data.object;
+                if (checkoutSessionCompleted.payment_status === 'paid') {
+                    // console.log('Payment for session completed successfully');
+                    const checkoutSession = await stripe.checkout.sessions.retrieve(
+                        checkoutSessionCompleted.id,
+                        {
+                            expand: ['line_items']
+                        }
+                    );
+                    const lineItems = checkoutSession.line_items?.data || [];
+                    // if (checkoutSession.metadata) {
+                    //     console.log('checkoutSession', checkoutSession)
+                    //     console.log('checkoutSession.metadata', checkoutSession.metadata)
+                    //     console.log('metadata.metadata', checkoutSession.metadata.metadata)
+                    // }
+                    console.log('lineItems are', lineItems)
+
+                    await dbConnect();
+                    const userId = checkoutSession.metadata?.userId;
+                    const user = await User.findById(userId);
+
+                    if (Array.isArray(user.cart)) {
+                        lineItems.forEach(item => {
+                            //after checkout, add product(s) to user
+                            user.subscriptionProducts.push({
+                                product: item.description,
+                                activationDate: null,
+                                expirationDate: null,
+                            })
+                            //after checkout, add activationCode to user
+                            const activationCode = generateActivationCode();
+                            user.activationInfos.push({
+                                code: activationCode,
+                                product: item.description,
+                                used: false,
+                            });
+                            //after checkout, delete the items in cart
+                            console.log('item.id is', item.id)
+                            user.cart = user.cart.filter((i: CartItemType) => i.name !== item.description);
+                        })
+                        await user.save();
+                    }
+                } else {
+                    console.log('no session')
+                }
+                break;
+            default:
+                console.log(`Unhandled event type: ${event.type}`);
+        }
+        return NextResponse.json({ received: true });
+
+    } catch (err: any) {
+        console.error('Error verifying webhook signature:', err);
+        return NextResponse.json({ message: `Webhook Error: ${err.message}` }, { status: 400 })
+    }
+} 
