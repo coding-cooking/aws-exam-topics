fb7c342bae6dea2f14c197a55827b61afe74047c
Coding-Cooking <fei.coding2023@gmail.com>
2025-01-04 14:13:50 +1100

    resolve the handler is not a valid Route export field issue

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
-export default async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
     const body = await req.json()
 
     const { email, activationCode } = body;
