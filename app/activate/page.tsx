9adddd26490e4d894152b1c3f20d3c7f5c925f14
Coding-Cooking <fei.coding2023@gmail.com>
2025-01-09 22:18:58 +1100

    build the activate page

---
+++ app/activate/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import ActivateForm from "@/components/ActicationForm"
+
+export default function ActivatePage() {
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <ActivateForm />
+        </div>
+
+    )
+}
