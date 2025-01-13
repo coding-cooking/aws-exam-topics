1542c29c5ad41c1f18bc70c9a08dd4e269509a2b
Coding-Cooking <fei.coding2023@gmail.com>
2025-01-13 21:55:31 +1100

    resolve the useSearchParams suspence issue

--- app/checkout/checkout-success/page.tsx
+++ app/checkout/checkout-success/page.tsx
@@ -3,6 +3,7 @@
 import { checkoutItemType } from "@/app/cart/page"
 import ActivateForm from "@/components/ActicationForm"
 import { useSearchParams } from "next/navigation"
+import { Suspense } from 'react'
 
 export default function CheckoutSuccess() {
     const searchParams = useSearchParams()
@@ -11,15 +12,18 @@ export default function CheckoutSuccess() {
     const itemNames = items.map((item: checkoutItemType) => item.name)
 
     return (
-        <div className="w-full h-full flex flex-col justify-center items-center">
-            <h1 className="mt-32 text-2xl text-teal-800 font-extrabold">Thank you!</h1>
-            <h2 className="text-lg">You have got access to <b>{itemNames.join(',')}</b></h2>
-            <div className="p-8">
-                <p>We have sent you an email with the activation code. </p>
-                <p>You can only get access to the product by submitting the code.</p>
-                <p>After activating the product, you will have the right to use this product for 1 year.</p>
+        <Suspense>
+            <div className="w-full h-full flex flex-col justify-center items-center">
+                <h1 className="mt-32 text-2xl text-teal-800 font-extrabold">Thank you!</h1>
+                <h2 className="text-lg">You have got access to <b>{itemNames.join(',')}</b></h2>
+                <div className="p-8">
+                    <p>We have sent you an email with the activation code. </p>
+                    <p>You can only get access to the product by submitting the code.</p>
+                    <p>After activating the product, you will have the right to use this product for 1 year.</p>
+                </div>
+                <ActivateForm />
             </div>
-            <ActivateForm />
-        </div>
+        </Suspense>
+
     )
 }
