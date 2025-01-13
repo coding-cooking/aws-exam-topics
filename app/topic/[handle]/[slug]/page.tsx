b099b37001129a5f27fe576dc2eda755f323857c
Coding-Cooking <fei.coding2023@gmail.com>
2025-01-13 21:43:07 +1100

    test

--- app/topic/[handle]/[slug]/page.tsx
+++ app/topic/[handle]/[slug]/page.tsx
@@ -1,46 +1,46 @@
-import { options } from "@/app/api/auth/[...nextauth]/options";
-import TopicTemplate from "@/components/TopicTemplate";
-import { TopicType } from "@/context/TopicsContext";
-import { getServerSession } from "next-auth";
-import { notFound } from "next/navigation";
+// import { options } from "@/app/api/auth/[...nextauth]/options";
+// import TopicTemplate from "@/components/TopicTemplate";
+// import { TopicType } from "@/context/TopicsContext";
+// import { getServerSession } from "next-auth";
+// import { notFound } from "next/navigation";
 
-export async function generateStaticParams() {
-    const topics: TopicType[] = await fetch(`${process.env.BASE_URL}/api/topic`)
-        .then((res) => res.json());
+// export async function generateStaticParams() {
+//     const topics: TopicType[] = await fetch(`${process.env.BASE_URL}/api/topic`)
+//         .then((res) => res.json());
 
-    if (!topics || topics.length === 0) {
-        return [];
-    } else {
-        return topics.map((topic) => ({
-            handle: topic.topicType,
-            slug: topic.topicId,
-        }))
-    }
-}
+//     if (!topics || topics.length === 0) {
+//         return [];
+//     } else {
+//         return topics.map((topic) => ({
+//             handle: topic.topicType,
+//             slug: topic.topicId,
+//         }))
+//     }
+// }
 
-export default async function TopicPage({ params }: { params: Promise<{ handle: string, slug: string }> }) {
-    const { handle, slug } = await params;
-    const session = await getServerSession(options);
+// export default async function TopicPage({ params }: { params: Promise<{ handle: string, slug: string }> }) {
+//     const { handle, slug } = await params;
+//     const session = await getServerSession(options);
 
-    if (!session?.user?.roles?.includes(`${handle}User`)) {
-        return notFound();
-    }
+//     if (!session?.user?.roles?.includes(`${handle}User`)) {
+//         return notFound();
+//     }
 
-    try {
-        const response = await fetch(`${process.env.BASE_URL}/api/topic/${handle}/${slug}`);
+//     try {
+//         const response = await fetch(`${process.env.BASE_URL}/api/topic/${handle}/${slug}`);
 
-        if (!response.ok) {
-            return notFound();
-        }
+//         if (!response.ok) {
+//             return notFound();
+//         }
 
-        const topic: TopicType = await response.json();
+//         const topic: TopicType = await response.json();
 
-        if (!topic) return notFound();
+//         if (!topic) return notFound();
 
-        return (<TopicTemplate topic={topic} />);
-    } catch (err) {
-        console.error("Error fetching topic:", err);
-        return notFound();
-    }
-}
+//         return (<TopicTemplate topic={topic} />);
+//     } catch (err) {
+//         console.error("Error fetching topic:", err);
+//         return notFound();
+//     }
+// }
 
