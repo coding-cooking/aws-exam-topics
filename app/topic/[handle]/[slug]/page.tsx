992a3e4da4d2aa7fb90a0154875cab2e3775566b
Coding-Cooking <fei.coding2023@gmail.com>
2025-01-15 14:30:10 +1100

    debug

--- app/topic/[handle]/[slug]/page.tsx
+++ app/topic/[handle]/[slug]/page.tsx
@@ -7,9 +7,14 @@ import { notFound } from "next/navigation";
 export async function generateStaticParams() {
     const topics: TopicType[] = await fetch(`${process.env.BASE_URL}/api/topic`)
         .then((res) => res.json());
+    console.log('fetch generateStaticParams from ', `${process.env.BASE_URL}/api/topic`)
     if (!topics || topics.length === 0) {
         return [];
     } else {
+        console.log('Generated params:', topics.map(t => ({
+            handle: t.topicType,
+            slug: t.topicId,
+        })));
         return topics.map((topic) => ({
             handle: topic.topicType,
             slug: topic.topicId,
@@ -20,6 +25,8 @@ export async function generateStaticParams() {
 export default async function TopicPage({ params }: { params: Promise<{ handle: string, slug: string }> }) {
     const { handle, slug } = await params;
     const session = await getServerSession(options);
+    console.log('Params received:', { handle, slug });
+    console.log('Session:', session);
 
     if (!session?.user?.roles?.includes(`${handle}User`)) {
         return notFound();
@@ -27,7 +34,7 @@ export default async function TopicPage({ params }: { params: Promise<{ handle: 
 
     try {
         const response = await fetch(`${process.env.BASE_URL}/api/topic/${handle}/${slug}`);
-
+        console.log('fetch TopicPage', `${process.env.BASE_URL}/api/topic/${handle}/${slug}`)
         if (!response.ok) {
             return notFound();
         }
